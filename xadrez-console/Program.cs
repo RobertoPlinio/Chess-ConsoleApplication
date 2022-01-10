9d1daac750b68eb1a1ccbfa9f6aa3008d834ad94
RobertoPlinio <robertoppjr@gmail.com>
2022-01-10 20:45:47 -0300

    Created Position class

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using Board;
 
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Position P = new Position(3, 4);
+            Console.WriteLine($"Position: {P}");
         }
     }
 }
